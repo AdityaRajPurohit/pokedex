3ba1ad24a16d9d5d7eb874d64b685b2db44a5ed4
AdityaRaj Purohit <purohit.adiraj30@gmail.com>
2019-07-10 14:57:52 +0530

    Updates

---
+++ precache-manifest.394434ed020d63e70523abbc75f5a839.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48372dc86a3dbf470ae5b46b00b8fcfb",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "1ff9b53b98de626c8036",
+    "url": "/pokedex/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "d63e0439906b391edfe7",
+    "url": "/pokedex/static/css/main.757ce06f.chunk.css"
+  },
+  {
+    "revision": "1ff9b53b98de626c8036",
+    "url": "/pokedex/static/js/2.3b3efb37.chunk.js"
+  },
+  {
+    "revision": "d63e0439906b391edfe7",
+    "url": "/pokedex/static/js/main.b2d11698.chunk.js"
+  },
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  },
+  {
+    "revision": "f09ba51b11d13d048de9afb8d2ce95e1",
+    "url": "/pokedex/static/media/large.f09ba51b.jpg"
+  },
+  {
+    "revision": "f1910ad9cc810f6d9c70faad016a83e0",
+    "url": "/pokedex/static/media/pokeball.f1910ad9.gif"
+  }
+]);
